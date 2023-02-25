590f5c7a9bcce917dca84aa7d5e80bdd676c5ce0
Harish1651999 <hk1651999@gmail.com>
2023-02-25 19:28:41 +0530

    add react fragment.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from "react";
 import "./App.css";
 import BackwardCounter from "./components/BackwardCounter";
 import ForwardCounter from "./components/ForwardCounter";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <BackwardCounter />
       <ForwardCounter />
-    </>
+    </React.Fragment>
   );
 }
 
